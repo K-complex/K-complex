1731e91ed110fc6582284295deb10e6a9b5f3c2a
Steven <steven_harland@hotmail.co.uk>
2017-02-11 14:49:56 +0000

    Rename app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
-import { MyApp } from './app.component';
+import { AppComponent } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DreamFormPage } from '../pages/dream-form/dream-form';
 import { DreamService } from './dream.service';
 
 @NgModule({
     declarations: [
-        MyApp,
+        AppComponent,
         HomePage,
         DreamFormPage
     ],
     imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(AppComponent)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
-        MyApp,
+        AppComponent,
         HomePage,
         DreamFormPage
     ],
